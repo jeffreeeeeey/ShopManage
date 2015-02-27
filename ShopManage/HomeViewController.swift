321d2e033b8e1bca14444525bd99e1bf4b74e78c
jeffreeeeeey <jeffreeeeeey@gmail.com>
2015-02-27 12:12:57 +0800

    first commit

---
+++ ShopManage/HomeViewController.swift
@@ -0,0 +1,46 @@
+//
+//  HomeViewController.swift
+//  ShopManage
+//
+//  Created by mac on 1/30/15.
+//  Copyright (c) 2015 llzg. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(true)
+        
+        let prefs:NSUserDefaults = NSUserDefaults.standardUserDefaults()
+        let isLoggedIn:Int = prefs.integerForKey("ISLOGGEDIN") as Int
+        if (isLoggedIn != 1) {
+            //LoginViewController vc = self.storyboard?.instantiateViewControllerWithIdentifier("loginVC")
+            let loginVC = self.storyboard?.instantiateViewControllerWithIdentifier("loginVC");
+        }
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ShopManage/User.swift
@@ -0,0 +1,11 @@
+//
+//  User.swift
+//  ShopManage
+//
+//  Created by mac on 2/27/15.
+//  Copyright (c) 2015 llzg. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
