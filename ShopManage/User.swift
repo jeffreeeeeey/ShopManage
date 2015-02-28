853e2bc666ab1b9a233e2ec9a3b86648b710231c
jeffreeeeeey <jeffreeeeeey@gmail.com>
2015-02-28 12:54:04 +0800

    add user manage

--- ShopManage/User.swift
+++ ShopManage/User.swift
@@ -9,3 +9,37 @@
 import Foundation
 import UIKit
 
+class User {
+    class var sharedUser : User {
+        struct Singleton {
+            static let instance = User()
+        }
+        return Singleton.instance;
+    }
+    private(set) var users: [String]
+    
+    init() {
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let storedUser = defaults.objectForKey("user") as? [String]
+        users = storedUser != nil ? storedUser! : []
+    }
+    
+    func addUser(userName: String) {
+        users[0] = userName
+        saveUsers()
+    }
+    
+    func removeUser(userName: String) {
+        if let index = find(users, userName) {
+            users.removeAtIndex(index)
+            saveUsers()
+        }
+    }
+    
+    func saveUsers() {
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject(users, forKey: "users")
+        defaults.synchronize()
+    }
+}
